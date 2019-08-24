6f37c177e8b553dc03d7975cff4f4bff4c4427d5
pritamkhose <pritamkhose@gmaio.com>
2019-08-24 23:43:34 +0530

    MVVM ViewModel Repos

--- app/src/main/java/com/pritam/mvvm/MainActivity.java
+++ app/src/main/java/com/pritam/mvvm/MainActivity.java
@@ -1,31 +1,35 @@
 package com.pritam.mvvm;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.pritam.mvvm.adapters.RecyclerAdapter;
-import com.pritam.mvvm.models.NicePlace;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ProgressBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pritam.mvvm.adapters.RecyclerAdapter;
+import com.pritam.mvvm.models.NicePlace;
+import com.pritam.mvvm.viewmodels.MainActivityViewModel;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private FloatingActionButton mFab;
     private RecyclerView mRecyclerView;
     private RecyclerAdapter mAdapter;
     private ProgressBar mProgressBar;
-    private ArrayList<NicePlace> dataSet = new ArrayList<>();
+    private MainActivityViewModel mMainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +42,43 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.recycler_view);
         mProgressBar = findViewById(R.id.progress_bar);
 
-        mFab.setOnClickListener(new View.OnClickListener() {
+        mMainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+
+        mMainActivityViewModel.init();
+
+        mMainActivityViewModel.getNicePlaces().observe(this, new Observer<List<NicePlace>>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onChanged(@Nullable List<NicePlace> nicePlaces) {
+                mAdapter.notifyDataSetChanged();
             }
         });
 
-        setNicePlaces();
+        mMainActivityViewModel.getIsUpdating().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                if(aBoolean){
+                    showProgressBar();
+                }
+                else{
+                    hideProgressBar();
+                    mRecyclerView.smoothScrollToPosition(mMainActivityViewModel.getNicePlaces().getValue().size()-1);
+                }
+            }
+        });
+
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMainActivityViewModel.addNewValue(
+                        new NicePlace(
+                                "https://i.imgur.com/ZcLLrkY.jpg",
+                                "Washington"
+                        )
+                );
+            }
+        });
+
         initRecyclerView();
     }
 
@@ -74,45 +106,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRecyclerView(){
-        mAdapter = new RecyclerAdapter(this, dataSet);
+        mAdapter = new RecyclerAdapter(this, mMainActivityViewModel.getNicePlaces().getValue());
         RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-    }
-
-    private void setNicePlaces(){
-        dataSet.add(
-                new NicePlace("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg",
-                        "Havasu Falls")
-        );
-        dataSet.add(
-                new NicePlace("https://i.redd.it/tpsnoz5bzo501.jpg",
-                        "Trondheim")
-        );
-        dataSet.add(
-                new NicePlace("https://i.redd.it/qn7f9oqu7o501.jpg",
-                        "Portugal")
-        );
-        dataSet.add(
-                new NicePlace("https://i.redd.it/j6myfqglup501.jpg",
-                        "Rocky Mountain National Park")
-        );
-        dataSet.add(
-                new NicePlace("https://i.redd.it/0h2gm1ix6p501.jpg",
-                        "Havasu Falls")
-        );
-        dataSet.add(
-                new NicePlace("https://i.redd.it/k98uzl68eh501.jpg",
-                        "Mahahual")
-        );
-        dataSet.add(
-                new NicePlace("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg",
-                        "Frozen Lake")
-        );
-        dataSet.add(
-                new NicePlace("https://i.redd.it/obx4zydshg601.jpg",
-                        "Austrailia")
-        );
     }
 
     private void showProgressBar(){
